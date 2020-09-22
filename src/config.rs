969ff25e859e6c3ec622a6a3bb27f9d4fb959d04
Bradley Bonitatibus <bradbonitatibus@gmail.com>
2020-09-22 14:20:53 -0400

    add some test + ci

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,7 @@ impl Config {
     pub fn new() -> Config {
         let mut args: Vec<String> = std::env::args().collect();
         args.remove(0); // remove program name from argument list
-        let is_valid = validate_args(&args);
+        let is_valid = validate_number_of_arguments(&args);
         if !is_valid {
             panic!("Invalid command line arguments passed");
         }
@@ -54,9 +54,33 @@ impl Config {
     }
 }
 
-fn validate_args(args: &Vec<String>) -> bool {
+fn validate_number_of_arguments(args: &Vec<String>) -> bool {
     if args.len() < 6 {
         return false;
     }
     return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{validate_number_of_arguments};
+    #[test]
+    fn test_failed_validation() {
+        let t : Vec<String> = vec![];
+        assert_ne!(validate_number_of_arguments(&t), true);
+    }
+
+    #[test]
+    fn test_validation_with_correct_number_of_args() {
+        let t : Vec<String> = vec![
+            String::from("0.05"),
+            String::from("0.0001"),
+            String::from("100",),
+            String::from("10000"),
+            String::from("1000",),
+            String::from("0"),
+        ];
+
+        assert!(validate_number_of_arguments(&t));
+    }
 }
